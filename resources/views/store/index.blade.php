07c5bda06045fe0d57c5892e589957c65215baf3
Johny Prieto <johnyprieto001@gmail.com>
2022-12-13 13:48:57 -0500

    feature: add the basic views to show products

---
+++ resources/views/store/index.blade.php
@@ -0,0 +1,16 @@
+@extends('welcome')
+
+@section('content')
+    <h2 class="mt-4">Productos en venta</h2>
+    <div class="row">
+        <div class="col-3 mt-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-sm btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
