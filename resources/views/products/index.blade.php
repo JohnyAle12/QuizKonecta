2a04cd6e9d17956595c5f397d7aeac36024f95a1
Johny Prieto <johnyprieto001@gmail.com>
2022-12-13 18:04:44 -0500

    add request and list products created

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,15 +8,33 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-3 mt-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-sm btn-success">Editar</a>
-                    <a href="#" class="btn btn-sm btn-danger">Eliminar</a>
+        @foreach ($products as $product)
+            <div class="col-3 mt-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ ucfirst($product->name) }}</h5>
+                        <p class="card-text">
+                            {{ ucfirst($product->reference) }}
+                            <ul>
+                                <li>
+                                    Stock: {{ $product->stock }} 
+                                    <small>Unidades</small>
+                                </li>
+                                <li>
+                                    Precio: ${{ $product->price }}
+                                    <small>COP</small>
+                                </li>
+                                <li>
+                                    Peso: {{ $product->weight }}
+                                    <small>Gr</small>
+                                </li>
+                            </ul>
+                        </p>
+                        <a href="#" class="btn btn-sm btn-success">Editar</a>
+                        <a href="#" class="btn btn-sm btn-danger">Eliminar</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 @endsection
