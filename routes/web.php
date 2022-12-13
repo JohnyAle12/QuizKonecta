24c482fbe414296d2230c62cae15341bd1c7445d
Johny Prieto <johnyprieto001@gmail.com>
2022-12-13 13:46:44 -0500

    feature: add controller and basic routes to show products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'))->name('welcome');
+Route::resource('products', ProductController::class)->names('products');
+Route::get('store', [StoreController::class, 'index'])->name('store.index');
